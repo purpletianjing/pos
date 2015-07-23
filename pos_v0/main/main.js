c554604c8e169d8fe2b61f04deefb4265ca573a8
purple <purpletianjing@163.com>
2015-07-23 17:45:20 +0800

    standard form

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,13 +1,37 @@
-function printReceipt(inputs) {
+function countMoneySum(inputs) {
+  var sum = 0;
+  inputs.forEach(function(val) {
+    sum += val.count * val.price;
+  });
+  return sum;
+}
+
+function concatGoodsInfo(inputs, profit) {
+  inputs.forEach(function(item) {
+    profit += "名称：" + item.name + "，数量：" + item.count +
+      item.unit + "，单价：" + item.price.toFixed(2) + "(元)，小计：" +
+      (item.count * item.price).toFixed(2) + "(元)\n";
+  });
+  return profit;
+}
+
+function concatReceiptInfo(inputs) {
   var profit = "***<没钱赚商店>收据***\n";
   var sum = 0;
-  inputs.forEach(function(item) {
-    sum += item.count * item.price;
-    profit += "名称：" + item.name + "，数量：" + item.count + item.unit +"，单价：" + item.price.toFixed(2) + "(元)，小计：" + (item.count * item.price).toFixed(2) + "(元)\n";
-  });
-  sum = sum.toFixed(2);
-  profit += "----------------------\n";
-  profit += "总计：" + sum + "(元)\n";
-  profit += "**********************";
+  sum = countMoneySum(inputs);
+  profit = concatGoodsInfo(inputs, profit);
+  profit = concatProfit(profit, sum);
+  return profit;
+}
+
+function concatProfit(profit, sum) {
+  profit += "----------------------\n" +
+    "总计：" + sum.toFixed(2) + "(元)\n" +
+    "**********************";
+  return profit;
+}
+
+function printReceipt(inputs) {
+  var profit = concatReceiptInfo(inputs);
   console.log(profit);
 }
