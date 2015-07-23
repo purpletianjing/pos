ff6b8a9d8408325bcfe61c260837245bf747d573
purple <purpletianjing@163.com>
2015-07-23 18:07:20 +0800

    commit test

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,43 +1,61 @@
-function mergeSameElements(val,result) {
-  // var sign = 0;
-  // result.forEach(function(item) {
-  //   if(item.name == val.name) {
-  //     item.count++;
-  //     item.priceSum += val.price;
-  //     sign = 1;
-  //   }
-  // });
-  // if(sign === 0) {
-  //   result.push({name:val.name,count:1,unit:val.unit,price:val.price,priceSum:val.price});
-  // }
+function resultPush(val, result) {
+  result.push({
+    name: val.name,
+    count: 1,
+    unit: val.unit,
+    price: val.price,
+    priceSum: val.price
+  });
+}
+
+function mergeSameElements(val, result) {
   for (var i = 0; i < result.length; i++) {
-    if(result[i].name == val.name) {
+    if (result[i].name == val.name) {
       result[i].count++;
       result[i].priceSum += val.price;
       return;
     }
   }
-  result.push({name:val.name,count:1,unit:val.unit,price:val.price,priceSum:val.price});
-  return;
+  resultPush(val, result);
 }
 
-function produceResult(result,profit) {
+function countMoneySum(result) {
   var sum = 0;
   result.forEach(function(item) {
-    profit += "名称：" + item.name + "，数量：" + item.count + item.unit + "，单价：" +
-              item.price.toFixed(2) + "(元)，小计：" + item.priceSum.toFixed(2) + "(元)\n";
     sum += item.priceSum;
   });
-  profit += "----------------------\n" + "总计：" + sum.toFixed(2) + "(元)\n**********************";
+  return sum;
+}
+
+function concatGoodsInfo(result, profit) {
+  result.forEach(function(item) {
+    profit += "名称：" + item.name + "，数量：" + item.count + item.unit +
+      "，单价：" + item.price.toFixed(2) + "(元)，小计：" +
+      item.priceSum.toFixed(2) + "(元)\n";
+  });
+  return profit;
+}
+
+function concatProfit(profit, sum) {
+  profit += "----------------------\n" +
+    "总计：" + sum.toFixed(2) + "(元)\n" +
+    "**********************";
+  return profit;
+}
+
+function concatReceiptInfo(result) {
+  var profit = "***<没钱赚商店>收据***\n";
+  var sum = countMoneySum(result);
+  profit = concatGoodsInfo(result, profit);
+  profit = concatProfit(profit, sum);
   return profit;
 }
 
 function printReceipt(inputs) {
-  var profit = "***<没钱赚商店>收据***\n";
   var result = [];
   inputs.forEach(function(val) {
-    mergeSameElements(val,result);
+    mergeSameElements(val, result);
   });
-  profit = produceResult(result,profit);
+  var profit = concatReceiptInfo(result);
   console.log(profit);
 }
--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,79 +1,159 @@
-function filterElements(val,allItems) {
-  var changeResult = {};
-  allItems.forEach(function(item) {
-    if(val.split('-')[0] == item.barcode) {
-      changeResult = {barcode:item.barcode,name:item.name,unit:item.unit,price:item.price,count:parseInt(val.split('-')[1] || 1)};
-    }
-  });
-  return changeResult;
-}
+// function filterElements(val, allItems) {
+//   var changeResult = {};
+//
+//   allItems.forEach(function(item) {
+//     if (val.split('-')[0] == item.barcode) {
+//       changeResult = {
+//         barcode: item.barcode,
+//         name: item.name,
+//         unit: item.unit,
+//         price: item.price,
+//         count: parseInt(val.split('-')[1] || 1)
+//       };
+//     }
+//   });
+//
+//   return changeResult;
+// }
+//
+// function countSameElements(element) {
+//   var result = [];
+//   for (var i = 0; i < result.length; i++) {
+//     if (result[i].name == element.name) {
+//       result[i].count++;
+//       result[i].priceSum += element.price * element.count;
+//       return;
+//     }
+//   }
+//
+//   result.push({
+//     barcode: element.barcode,
+//     name: element.name,
+//     count: element.count,
+//     unit: element.unit,
+//     price: element.price,
+//     priceSum: (element.price * element.count)
+//   });
+//   return result;
+// }
+//
+// function isPromotions(val, giveGoods) {
+//   promotionBarcode = loadPromotions();
+//
+//   promotionBarcode[0].barcodes.forEach(function(item) {
+//     if ((val.count > 2) && (val.barcode == item)) {
+//       val.priceSum -= parseInt(val.count / 3) * val.price;
+//       giveGoods.push({
+//         name: val.name,
+//         count: parseInt(val.count / 3),
+//         unit: val.unit,
+//         priceSum: parseInt(val.count / 3) * val.price
+//       });
+//     }
+//   });
+//
+//   return giveGoods;
+// }
+//
+// function countGiveGoods(result, giveGoods) {
+//   result.forEach(function(val) {
+//     isPromotions(val, giveGoods);
+//   });
+// }
+//
+// function outputGiveReceipt(outputStr, giveGoods, sum) {
+//   var outputSum = 0;
+//   outputStr += '----------------------\n' + '挥泪赠送商品：\n';
+//
+//   giveGoods.forEach(function(item) {
+//     outputStr += '名称：' + item.name + '，数量：' + item.count + item.unit + '\n';
+//     outputSum += item.priceSum;
+//   });
+//   outputStr += '----------------------\n';
+//   outputStr += '总计：' + sum.toFixed(2) + '(元)\n' + '节省：' + outputSum.toFixed(2) +
+//     '(元)\n' + '**********************';
+//
+//   return outputStr;
+// }
+//
+// function outputReceipt(outputStr, result, giveGoods) {
+//   var sum = 0;
+//
+//   result.forEach(function(val) {
+//     outputStr += '名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' +
+//       val.price.toFixed(2) + '(元)，小计：' + val.priceSum.toFixed(2) + '(元)\n';
+//     sum += val.priceSum;
+//   });
+//
+//   outputStr = outputGiveReceipt(outputStr, giveGoods, sum);
+//
+//   return outputStr;
+// }
+//
+// function printReceipt(inputs) {
+//   allItems = loadAllItems();
+//   var result = [];
+//   var giveGoods = [];
+//   var outputStr = '***<没钱赚商店>收据***\n';
+//
+//   result = inputs.map(function(val) {
+//     return filterElements(val, allItems);
+//   }).forEach(function(element) {
+//     countSameElements( element);
+//   });
+//
+//   countGiveGoods(result, giveGoods);
+//   outputStr = outputReceipt(outputStr, result, giveGoods);
+//
+//   console.log(outputStr);
+// }
 
-function countSameElements(result,element) {
-  for (var i = 0; i < result.length; i++) {
-    if(result[i].name == element.name) {
-      result[i].count++;
-      result[i].priceSum += element.price * element.count;
-      return;
+
+
+
+
+
+
+function mapInputs(val) {
+  var splitResult = val.split('-');
+  allItems = loadAllItems();
+  for (var i = 0; i < allItems.length; i++) {
+    if (allItems[i].barcode === splitResult[0]) {
+      return {
+        barcode: allItems[i].barcode,
+        name: allItems[i].name,
+        count: splitResult[1] || 1,
+        price: allItems[i].price,
+        realMoney: (splitResult[1] || 1) * allItems[i].price
+      };
     }
   }
-  result.push({barcode:element.barcode,name:element.name,count:element.count,
-              unit:element.unit,price:element.price,priceSum:(element.price * element.count)});
-  return;
 }
 
-function isPromotions(val,giveGoods) {
-  promotionBarcode = loadPromotions();
-  promotionBarcode[0].barcodes.forEach(function(item) {
-    if((val.count > 2) && (val.barcode == item)) {
-      val.priceSum -= parseInt(val.count / 3) * val.price;
-      giveGoods.push({name:val.name,count:parseInt(val.count / 3),unit:val.unit,
-                      priceSum:parseInt(val.count / 3) * val.price});
-    }
-  });
-  return giveGoods;
+function promotionSaveMoney(val) {
+  val.realSum = val.count * val.money;
+  if(val.count > 2 ) {
+    val.saveMoney = 5;
+  }
 }
 
-function countGiveGoods(result,giveGoods) {
-  result.forEach(function(val) {
-    isPromotions(val,giveGoods);
-  });
-}
-
-function outputGiveReceipt(outputStr,giveGoods,sum) {
-  var outputSum = 0;
-  outputStr += '----------------------\n' + '挥泪赠送商品：\n';
-  giveGoods.forEach(function(item) {
-    outputStr += '名称：' + item.name + '，数量：' + item.count + item.unit + '\n';
-    outputSum += item.priceSum;
-  });
-  outputStr += '----------------------\n';
-  outputStr += '总计：' + sum.toFixed(2) + '(元)\n' + '节省：' + outputSum.toFixed(2) +
-               '(元)\n' + '**********************';
-  return outputStr;
-}
-
-function outputReceipt(outputStr,result,giveGoods) {
-  var sum = 0;
-  result.forEach(function(val) {
-    outputStr += '名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' +
-                val.price.toFixed(2) + '(元)，小计：' + val.priceSum.toFixed(2) + '(元)\n';
-    sum += val.priceSum;
-  });
-  outputStr = outputGiveReceipt(outputStr,giveGoods,sum);
-  return outputStr;
+function countMapResult(mapResult) {
+  var countResult = [];
+  //mapResult.forEach(function(val)
+  for(var i = 0; i < mapResult.length; i++){    
+    countResult.forEach(function(item) {
+      if(mapResult[i].barcode === item.barcode) {
+        item.count += mapResult[i].count;
+        item.realMoney += mapResult[i].count * item.price;
+      }
+    });
+  }
 }
 
 function printReceipt(inputs) {
-  allItems = loadAllItems();
-  var result = [];
-  var giveGoods = [];
-  var outputStr = '***<没钱赚商店>收据***\n';
-  inputs.map(function(val) {
-    return filterElements(val,allItems);
-  }).forEach(function(element) {
-    countSameElements(result,element);
+  var mapResult = inputs.map(function(val) {
+    return mapInputs(val);
   });
-  countGiveGoods(result,giveGoods);
-  outputStr = outputReceipt(outputStr,result,giveGoods);
-  console.log(outputStr);
+  countMapResult(mapResult);
+  console.log(mapResult);
 }
